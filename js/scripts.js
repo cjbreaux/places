0e5c909927f8d7cdc01d7eef373e8964839ce444
Chris Breaux and Joe Pritchett <joe@>
2019-01-28 16:59:01 -0800

    begin to add functionality to webpage

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var destinationArray = [];
+
+
 function Destination(location, landmarks, season, notes) {
   this.location = location;
   this.landmarks = landmarks;
@@ -9,9 +12,23 @@ function Destination(location, landmarks, season, notes) {
 $(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
-    var dest1 = new Destination()
-    $("#placesList").prepend()
-  })
+    var location = $("#place").val();
+    var landmark = $("#landmark").val();
+    var season = $("#season").val();
+    var note = $("#note").val();
+    //var name = $("#objectName").val();
+    var increment = 0;
+    console.log(increment)
+    var name = new Destination(location, landmark, season, note);
+    destinationArray.push(name);
+    $("#placesList").append("<li>" + destinationArray[increment].location + "</li>");
 
+    $("#placesList").click(function(){
+      $("#info").text(destinationArray[increment].location);
+      $("#info").append( destinationArray[increment].landmarks);
+      $("#info").append("<li>" + destinationArray[increment].season + "</li>");
+      $("#info").append("<li>" + destinationArray[increment].notes + "</li>");
 
-})
+    });
+  });
+});
